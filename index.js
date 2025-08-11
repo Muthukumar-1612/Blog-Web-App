bd3a49db80580b38acebcfe50df779e03ebc4bdb
MUTHUKUMAR <mugimugiff@gmail.com>
2025-08-11 21:29:44 +0530

    Changes in upload image local disk to cloud

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { db } from "./server/db.js"
 import multer from "multer";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -23,16 +29,35 @@ const day = date.getDate();
 const year = date.getFullYear();
 const formatted = `${monthName}-${day}-${year}`;
 
-// Store the upload file in specfic path
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, "Public/uploads"));
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
+// // Store the upload file in specfic path
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(__dirname, "Public/uploads"));
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+//         cb(null, uniqueSuffix + path.extname(file.originalname));
+//     }
+// })
+
+
+// Cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Cloudinary storage for multer
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "blog_uploads",
+        allowed_formats: ["jpg", "jpeg", "png"],
+        public_id: (req, file) => Date.now()
     }
-})
+});
+
 const upload = multer({ storage: storage })
 
 app.use((req, res, next) => {
