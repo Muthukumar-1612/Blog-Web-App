2ae42be9bd5edd4bd12e06585aa08ed00326c1f2
MUTHUKUMAR <mugimugiff@gmail.com>
2025-08-11 12:40:32 +0530

    Port change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { dirname } from 'path';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("Public"));
 app.use(express.urlencoded({ extended: true }));
