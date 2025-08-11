3fba4869dc356273f09c0b279d89121bee6ac8e8
MUTHUKUMAR <mugimugiff@gmail.com>
2025-08-11 14:56:34 +0530

    db changes

--- server/db.js
+++ server/db.js
@@ -7,10 +7,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 env.config({ path: path.join(__dirname, ".env") });
 
+// Pick which DB config to use
+const isRender = process.env.DB_ENV === "render";
+
 export const db = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-})
+    user: isRender ? process.env.RENDER_PG_USER : process.env.LOCAL_PG_USER,
+    host: isRender ? process.env.RENDER_PG_HOST : process.env.LOCAL_PG_HOST,
+    database: isRender ? process.env.RENDER_PG_DATABASE : process.env.LOCAL_PG_DATABASE,
+    password: isRender ? process.env.RENDER_PG_PASSWORD : process.env.LOCAL_PG_PASSWORD,
+    port: isRender ? process.env.RENDER_PG_PORT : process.env.LOCAL_PG_PORT,
+    ssl: isRender ? { rejectUnauthorized: false } : false // SSL only for Render
+});
