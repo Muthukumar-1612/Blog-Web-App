96d0cc5921af5730e7f7aa44feef858bfd3ac97b
MUTHUKUMAR <mugimugiff@gmail.com>
2025-08-11 15:47:02 +0530

    updated DB config 2

--- server/db.js
+++ server/db.js
@@ -7,14 +7,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 env.config({ path: path.join(__dirname, ".env") });
 
-// Pick which DB config to use
+dotenv.config();
+
 const isRender = process.env.DB_ENV === "render";
+const connectionString = isRender
+    ? process.env.RENDER_DB_URL
+    : process.env.LOCAL_DB_URL;
 
 export const db = new Pool({
-    user: isRender ? process.env.RENDER_PG_USER : process.env.LOCAL_PG_USER,
-    host: isRender ? process.env.RENDER_PG_HOST : process.env.LOCAL_PG_HOST,
-    database: isRender ? process.env.RENDER_PG_DATABASE : process.env.LOCAL_PG_DATABASE,
-    password: isRender ? process.env.RENDER_PG_PASSWORD : process.env.LOCAL_PG_PASSWORD,
-    port: isRender ? process.env.RENDER_PG_PORT : process.env.LOCAL_PG_PORT,
-    ssl: isRender ? { rejectUnauthorized: false } : false // SSL only for Render
+    connectionString,
+    ssl: isRender ? { rejectUnauthorized: false } : false
 });
