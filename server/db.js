0014767adb1f43bd2728398df13cd6257e9f6f33
Muthukumar-1612 <muthukumar16122002@gmail.com>
2025-08-28 11:22:34 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -7,13 +7,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 env.config({ path: path.join(__dirname, ".env") });
 
-const isRender = process.env.DB_ENV === "render";
-const connectionString = isRender
-    ? process.env.RENDER_DB_URL
-    : process.env.LOCAL_DB_URL;
-
+const isProd = process.env.NODE_ENV === "production";
 export const db = new Pool({
-    connectionString,
-    ssl: isRender ? { rejectUnauthorized: false } : false
+    connectionString: isProd ? process.env.NEON_DB_URL : process.env.LOCAL_DB_URL,
+    ssl: isProd ? {
+        rejectUnauthorized: false,
+        sslmode: 'require'
+    } : false
 });
+
 
